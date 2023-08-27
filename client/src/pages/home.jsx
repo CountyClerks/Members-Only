e96b5c07c0c31941867e1fb4ca996606ccb14e0c
Eric Hardy <erichardy011@gmail.com>
2023-08-26 22:08:02 -0500

    Add navbar to home page

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/navbar"
+export default function Home() {
+    return (
+        <main>
+            <section classname="nav">
+                <NavBar></NavBar>
+            </section>
+        </main>
+    )
+}
