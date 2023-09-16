744875f94b18989aee97bb691dc1ca39ab7c4c1e
Eric Hardy <erichardy011@gmail.com>
2023-09-16 04:44:21 -0500

    added react router for redirection handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import './home.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from'./pages/home'
+import Login from './pages/login'
+import SignUp from './pages/signup'
+import NewMessage from './pages/newMessage'
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/new-message" element={<NewMessage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
