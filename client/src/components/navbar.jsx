56a50b40803d1152ff5bd0a8b9ca379ebee0a68a
Eric Hardy <erichardy011@gmail.com>
2023-09-17 17:02:41 -0500

    fix brand not linking to homepage

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -9,7 +9,7 @@ export default function NavBar() {
     return (
         <Navbar bg="primary" expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="">Member's Only</Navbar.Brand>
+                <Navbar.Brand href="/">Member's Only</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Nav className="me-auto">
                     <Nav.Link href="#signup">Sign Up</Nav.Link>
