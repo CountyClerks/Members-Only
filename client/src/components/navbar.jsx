b1fbd32a7ec28771a1e66a1f43dd479995017559
Eric Hardy <erichardy011@gmail.com>
2023-09-18 21:34:40 -0500

    fixed navbar redirection urls

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,19 +1,14 @@
 import '../nav.css'
 import { Navbar, Container, Nav} from 'react-bootstrap'
 export default function NavBar() {
-    // const modalChange = () => {
-        
-    // }
-
-
     return (
         <Navbar bg="primary" expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="/">Member's Only</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Nav className="me-auto">
-                    <Nav.Link href="#signup">Sign Up</Nav.Link>
-                    <Nav.Link href="#login">Log In</Nav.Link>
+                    <Nav.Link href="signup">Sign Up</Nav.Link>
+                    <Nav.Link href="login">Log In</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
