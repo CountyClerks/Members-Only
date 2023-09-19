a2e34f3771912932af261449e9f660fba4baeef5
Eric Hardy <erichardy011@gmail.com>
2023-09-18 21:36:57 -0500

    added navbar to all pages, and imported forms to respective pages

--- client/src/pages/newMessage.jsx
+++ client/src/pages/newMessage.jsx
@@ -1,6 +1,10 @@
+import NavBar from '../components/navbar'
+import NewMessageForm from '../components/newMessageForm'
 export default function NewMessage() {
     return (
         <>
+            <NavBar />
+            <NewMessageForm />
         </>
     )
 }
--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,7 +1,9 @@
+import NavBar from '../components/navbar'
 import SignUpForm from '../components/signUpForm'
 export default function SignUp() {
     return (
         <>
+            <NavBar />
             <SignUpForm />
         </>
     )
