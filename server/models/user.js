b52bc79a6e7079a037bb6f9ef008db0a67cba4f9
Eric Hardy <erichardy011@gmail.com>
2023-09-19 22:08:03 -0500

    add member_status and admin schema

--- server/models/user.js
+++ server/models/user.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     username: {type: String, required: true, maxlength: 30},
     password: {type: String, required: true},
-    member_status: {type: Boolean, required: false},
-    admin: {type: Boolean, requireD: false},
+    member_status: {type: Boolean},
+    admin: {type: Boolean},
 })
 
 module.exports = mongoose.model('User', UserSchema)
