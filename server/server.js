992f1043bd77f0777c88ea76264fc97eca9c493f
Eric Hardy <erichardy011@gmail.com>
2023-09-19 23:55:58 -0500

    import new npm packages, and add routes to all pages

--- server/server.js
+++ server/server.js
@@ -1,26 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/logIn')
+const newMessageRouter = require('./routes/newMessage')
+const signUpRouter = require('./routes/signUp')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const app = express();
 
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
 
+//Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/new-message', newMessageRouter);
+app.use('/signup', signUpRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
