81df0e8dbd83032f8ec241dd579624e8acced505
Eric Hardy <erichardy011@gmail.com>
2023-09-19 22:01:31 -0500

    import navbar component

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,9 @@
+import NavBar from '../components/navbar'
 import LogInForm from '../components/logInForm'
 export default function Login() {
     return (
         <>
+            <NavBar />
             <LogInForm />
         </>
     )
