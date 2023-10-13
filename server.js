fdd7845f2d6c3fad35bc9266ae8f0a871b430f25
Eric Hardy <erichardy011@gmail.com>
2023-10-13 02:37:59 -0500

    connection to mongoDB

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
+require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
+const path = require('path')
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
 const cors = require('cors')
 
 const indexRouter = require('./routes/index');
-const loginRouter = require('./routes/logIn')
-const newMessageRouter = require('./routes/newMessage')
-const signUpRouter = require('./routes/signUp')
+// const loginRouter = require('./routes/logIn')
+// const newMessageRouter = require('./routes/newMessage')
+// const signUpRouter = require('./routes/signUp')
 
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+main().catch((err) => console.log(err))
+async function main() {
+  await mongoose.connect(process.env.DB_URL)
+}
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(compression());
 
 //Routes
 app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/new-message', newMessageRouter);
-app.use('/signup', signUpRouter)
+// app.use('/login', loginRouter);
+// app.use('/new-message', newMessageRouter);
+// app.use('/signup', signUpRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,5 +55,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}`))
 
 module.exports = app;
