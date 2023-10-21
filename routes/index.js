85eb6d2db8057c1d0630b9e5425b9bc26e49f8f6
Eric Hardy <erichardy011@gmail.com>
2023-10-21 00:37:33 -0500

    clean up

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ const message_controller = require('../controllers/messageController')
 const user_controller = require('../controllers/userController')
 
 //GET home page
-
 router.get('/', message_controller.index)
 
+router.get('/new-message',)
+router.post('/new-message',)
+
+//Auth Routes
 router.get('/login', user_controller.log_in_get)
-router.post('/login', message_controller.index)
+router.post('/login', user_controller.log_in_post)
 
 router.get('/signup', user_controller.sign_up_get)
-router.post('/signup', message_controller.index)
-
-router.get('/new-message', message_controller.index)
-router.post('/new-message', message_controller.index)
+router.post('/signup', user_controller.sign_up_post)
 
 module.exports = router
